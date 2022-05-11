8c47c327f1558b61d1f6e42a5bea73313e51dced
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2022-05-11 13:14:16 +0400

    drop python 2.7 support and tests

--- setup.py
+++ setup.py
@@ -40,21 +40,13 @@ setup(
         'graphviz',
         'tabulate>=0.7.7',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
-    extras_require={
-        ":python_version<'3.6.0'": [
-            'singledispatch >= 3.4.0.3',
-            'typing',
-        ],
-    },
+    python_requires=">=3.6",
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
