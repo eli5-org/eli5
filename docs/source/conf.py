6c5eb2aa1cae4dc9a982d6ae9080404a024e449a
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2025-03-26 11:41:55 +0000

    bring back the RTD theme as it's not auto-applied

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -170,6 +170,11 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_rtype = False
